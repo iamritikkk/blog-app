2dc95d2afeb2d39eda7af56499d66f5d17ded991
Ritik Chauhan <ritikchauhan@mogiio.com>
2024-01-26 12:16:44 +0530

    connect to the db

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+mongoose.connect(process.env.DB_URI).then(() => {
+  console.log("db is connected !!!")
+}).catch((error) => {
+  console.log("error in connecting db", error)
+})
+
+
 
 app.use(express.json());
 app.use(morgan("dev"));
