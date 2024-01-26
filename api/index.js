27f222603e5988d00631119d3d5e23d41129b631
Ritik Chauhan <ritikchauhan@mogiio.com>
2024-01-26 14:47:58 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -3,28 +3,28 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import userRoutes from "./routes/user.route.js"
-
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-mongoose.connect(process.env.DB_URI).then(() => {
-  console.log("db is connected !!!")
-}).catch((error) => {
-  console.log("error in connecting db", error)
-})
-
-
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("db is connected !!!");
+  })
+  .catch((error) => {
+    console.log("error in connecting db", error);
+  });
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
-
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
